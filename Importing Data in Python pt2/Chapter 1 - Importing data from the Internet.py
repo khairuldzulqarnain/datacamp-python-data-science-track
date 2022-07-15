80ba86014e77e65276eee68e421b336fa1563ebd
Khairul Dzulqarnain <107108078+khairuldzulqarnain@users.noreply.github.com>
2022-07-15 21:05:46 +0800

    #Importing non-flat files from the web
    #Importing non-flat files from the web
    
    # Import package
    import pandas as pd
    
    # Assign url of file: url
    
    url = 'https://assets.datacamp.com/course/importing_data_into_r/latitude.xls'
    
    
    # Read in all sheets of Excel file: xls
    
    xls = pd.read_excel(url, sheet_name=None)
    
    # Print the sheetnames to the shell
    
    print(xls.keys())
    
    # Print the head of the first sheet (using its name, NOT its index)
    
    print(xls['1700'].head())

--- Importing Data in Python pt2/Chapter 1 - Importing data from the Internet.py
+++ Importing Data in Python pt2/Chapter 1 - Importing data from the Internet.py
@@ -40,21 +40,25 @@ plt.ylabel('count')
 plt.show()
 
 
-#Importing non-flat files from the web
 # Import package
 import pandas as pd
 
 # Assign url of file: url
-url = 'http://s3.amazonaws.com/assets.datacamp.com/course/importing_data_into_r/latitude.xls'
 
-# Read in all sheets of Excel file: xl
-xl = pd.read_excel(url, sheetname=None)
+url = 'https://assets.datacamp.com/course/importing_data_into_r/latitude.xls'
+
+
+# Read in all sheets of Excel file: xls
+
+xls = pd.read_excel(url, sheet_name=None)
 
 # Print the sheetnames to the shell
-print(xl.keys())
+
+print(xls.keys())
 
 # Print the head of the first sheet (using its name, NOT its index)
-print(xl['1700'].head())
+
+print(xls['1700'].head())
 
 ########################################################################
 #Performing HTTP requests in Python using urllib
